361f546d7a70139bfa2bcca13013b15378320f46
FriyMade <64865953+FriyMade@users.noreply.github.com>
2021-11-26 18:53:48 +0300

    Add files via upload

---
+++ Assets/scripts/GroupBox_script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupBox_script : MonoBehaviour
+{
+
+    public bool wasFirst = false;
+    public box_script FirstBox;
+    public box_script SecondBox;
+    Vector3 med;
+
+    public GameObject movementbox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void ChangeBox()
+    {
+        FirstBox.transform.position -= new Vector3(0, 2, 0);
+
+        med = SecondBox.transform.position;
+        SecondBox.transform.position = FirstBox.transform.position;
+        FirstBox.transform.position = med;
+
+        FirstBox.transform.position += new Vector3(0, 2, 0);
+        FirstBox.switcher();
+
+    } 
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+}
---
+++ Assets/scripts/star_script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class star_script : MonoBehaviour
+{
+    public GameObject Bug;
+    public GameObject Mesh;
+    public GameObject GameManager;
+    GameManager_script GameManager_script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager_script = GameManager.GetComponent<GameManager_script>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ChangeLevel(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        GameManager_script.nextLevel();
+        GameManager_script.bug_Script.UInotActive = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == Bug.GetComponent<BoxCollider>())
+        {
+            GameManager_script.bug_Script.UInotActive = false;
+            GetComponent<AudioSource>().Play();
+            GetComponent<ParticleSystem>().Play();
+            Mesh.SetActive(false);
+            StartCoroutine(ChangeLevel(1));
+        }
+    }
+}
